05b34dcbb5225ede0712c5f513bbca95886c9cb9
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-01-15 16:24:04 +0900

    update: Change root directory . to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
+    './src/components/**/*.{js,vue,ts}',
+    './src/layouts/**/*.vue',
+    './src/pages/**/*.vue',
+    './src/plugins/**/*.{js,ts}',
+    './src/middleware/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-    './app.vue',
+    './src/app.vue',
   ],
   theme: {
     extend: {},
