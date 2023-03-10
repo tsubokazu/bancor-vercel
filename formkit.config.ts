4de6183764788620162eac83422ace8a41a93e4b
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-03-10 14:22:10 +0900

    feature: 採用応募ページの雛形作成

--- formkit.config.ts
+++ formkit.config.ts
@@ -4,8 +4,6 @@ import { generateClasses } from '@formkit/themes';
 export default {
   config: {
     locales: { ja, en },
-    // アクティブなロケールを指定
-    locale: 'ja',
     classes: generateClasses({
       text: {
         inner:
@@ -18,7 +16,7 @@ export default {
       },
       textarea: {
         inner:
-          'w-full h-[116px] border border-bancor-gray600 rounded-lg overflow-hidden focus-within:border-bancor-blue100',
+          'w-full h-[116px] border border-bancor-gray600 rounded-lg overflow-hidden focus-within:border-bancor-blue100 py-3',
         input:
           'w-full h-[116px] px-3 border-none rounded-lg text-base text-bancor-black100 placeholder-gray-400  bg-bancor-gray1000',
         help: 'text-xs text-gray-500',
@@ -27,7 +25,7 @@ export default {
       },
       date: {
         inner:
-          'w-full border border-bancor-gray600 rounded-lg overflow-hidden focus-within:border-bancor-blue100',
+          'text-bancor-gray600 w-full border border-bancor-gray600 rounded-lg overflow-hidden focus-within:border-bancor-blue100',
         input:
           'w-full h-10 px-3 border-none rounded-lg text-base text-bancor-black100 placeholder-gray-400  bg-bancor-gray1000',
         help: 'text-xs text-gray-500',
@@ -61,6 +59,31 @@ export default {
         help: 'text-xs text-gray-500',
         messages: 'list-none p-0 mt-1 mb-0',
         message: 'text-red-500 mb-1 text-xs',
+      },
+      select: {
+        inner:
+          'text-bancor-gray600 w-full border border-bancor-gray600 rounded-lg overflow-hidden focus-within:border-bancor-blue100',
+        input:
+          'w-full h-10 px-3 border-none rounded-lg text-base text-bancor-black100 placeholder-gray-400  bg-bancor-gray1000',
+        placeholder: 'text-bancor-gray600',
+        help: 'text-xs text-gray-500',
+        messages: 'list-none p-0 mt-1 mb-0',
+        selection: 'text-bancor-gray200 px-3',
+        message: 'text-red-500 mb-1 text-xs',
+      },
+      file: {
+        inner: 'flex space-x-4 items-center',
+        fileItem: 'flex items-center justify-between space-x-2',
+        input:
+          'w-[174px] h-8 bg-bancor-gray600 border border-bancor-gray500 rounded-lg text-bancor-black100',
+        help: 'text-xs text-gray-500',
+        messages: 'list-none p-0 mt-1 mb-0',
+        message: 'text-red-500 mb-1 text-xs',
+      },
+      checkbox: {
+        wrapper: 'flex items-center space-x-3',
+        input:
+          'w-5 h-5 border border-bancor-gray600 rounded-lg flex items-center justify-center',
       },
     }),
   },
--- src/pages/entry.vue
+++ src/pages/entry.vue
@@ -11,6 +11,10 @@
     const node = myForm.value.node;
     node.submit();
   };
+  const inputFile = () => {
+    const node = myForm.value.node;
+    node.querySelector('input[type="file"]').click();
+  };
 
   definePageMeta({
     layout: 'entry',
@@ -22,6 +26,40 @@
     await pagesRecruitStore.fetchPagesRecruit();
   }
   const { entries }: RecruitObject = pagesRecruitStore;
+
+  // 直近の職歴の数
+  const recentHistoryCount = ref(1);
+  const clickAddRecentHistory = () => {
+    recentHistoryCount.value++;
+  };
+
+  const config: any = inject(Symbol.for('FormKitConfig'));
+  config.locale = 'ja';
+
+  // 郵便番号中のハイフンを削除
+  const removeHyphen = (zipCode: string) => {
+    return zipCode.replace(/-/g, '');
+  };
+
+  // 住所を取得してフォームにセット
+  const getAddress = async () => {
+    const node = myForm.value.node;
+    const zipCode = node.querySelector('input[name="zip_code"]').value;
+    console.log(zipCode);
+
+    const address = await fetch(
+      `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipCode}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status == 200) {
+          return data.results[0].address1 + data.results[0].address2;
+        } else {
+          return '';
+        }
+      });
+    node.querySelector('input[name="address"]').value = address;
+  };
 </script>
 
 <template>
@@ -46,7 +84,7 @@
       class="flex w-full flex-col items-center rounded-[10px] bg-white pt-10 pc:max-w-[1100px]"
     >
       <!-- 氏名 -->
-      <FormKit type="form" ref="myForm" :actions="false">
+      <FormKit type="form" ref="myForm" :actions="false" locale="ja">
         <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
           <!-- 姓 -->
           <FormKit
@@ -97,9 +135,9 @@
         <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
           <!-- 生年月日 -->
           <FormKit
-            type="text"
+            type="date"
             label="生年月日"
-            value="2000-01-01"
+            placeholder="2000/01/01"
             validation="required|date_before:2020-01-01"
           >
             <template #label="context">
@@ -175,6 +213,7 @@
           <div class="w-full tb:w-[420px] pc:w-[620px]">
             <FormKit
               type="text"
+              name="zipCode"
               label="郵便番号"
               placeholder="000-0000"
               validation="required|matches:^[0-9]{3}-[0-9]{4}$"
@@ -195,6 +234,7 @@
             class="flex w-full items-end tb:absolute tb:right-0 tb:top-10 tb:w-[180px] pc:w-[220px]"
           >
             <button
+              @click="getAddress"
               class="flex w-full items-center justify-center rounded-lg border border-bancor-gray600 py-2 text-base font-bold"
             >
               住所自動入力
@@ -208,6 +248,7 @@
           <FormKit
             type="text"
             label="住所"
+            name="address"
             placeholder="入力してください"
             validation="required"
           >
@@ -246,7 +287,7 @@
         <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
           <!-- 応募理由・自己PR -->
           <FormKit
-            type="text"
+            type="textarea"
             label="応募理由・自己PR"
             placeholder=""
             help="※400文字以内でご記入ください。"
@@ -261,12 +302,209 @@
             </template>
           </FormKit>
         </div>
+
+        <!-- 最終学歴 -->
+        <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
+          <AtomsFormBasicLabel text="最終学歴" :isRequired="false" class="mb-4">
+          </AtomsFormBasicLabel>
+          <!-- 在学期間・就学区分 -->
+          <div class="mb-6 flex items-center space-x-4">
+            <!-- 入学年月日 -->
+            <FormKit
+              type="text"
+              placeholder="2020/01"
+              inner-class="w-[160px] tb:w-[272px]"
+            >
+            </FormKit>
+            <div>〜</div>
+            <!-- 卒業年月日 -->
+            <FormKit
+              type="text"
+              placeholder="2022/12"
+              inner-class="w-[160px] tb:w-[272px]"
+            >
+            </FormKit>
+            <!-- 就学区分 -->
+            <FormKit
+              type="select"
+              name="educationType"
+              placeholder="就学区分"
+              inner-class="w-[360px] tb:w-[252px]"
+              :options="[
+                '高校',
+                '大学',
+                '大学院',
+                '専門学校',
+                '短期大学',
+                'その他',
+              ]"
+            >
+            </FormKit>
+          </div>
+          <!-- 学校名 -->
+          <div class="mb-6">
+            <FormKit type="text" placeholder="学校名" class="mb-6"></FormKit>
+          </div>
+          <!-- 学部 -->
+          <div class="mb-6">
+            <FormKit type="text" placeholder="学部" class="mb-6"></FormKit>
+          </div>
+          <!-- 学科名・研究室名 -->
+          <div class="mb-6">
+            <FormKit
+              type="text"
+              placeholder="学科名・研究室名"
+              class="mb-6"
+            ></FormKit>
+          </div>
+          <!-- 備考 -->
+          <div class="mb-6">
+            <FormKit type="text" placeholder="備考" class="mb-6"></FormKit>
+          </div>
+        </div>
+
+        <!-- 直近の職歴 -->
+        <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
+          <!-- 各職歴 -->
+          <div
+            v-for="recentHistoryNum in recentHistoryCount"
+            class="mb-8 w-full"
+            :key="recentHistoryNum"
+          >
+            <AtomsFormBasicLabel
+              :text="
+                recentHistoryCount > 1
+                  ? `直近の職歴 (${recentHistoryNum}/${recentHistoryCount})`
+                  : '直近の職歴'
+              "
+              :isRequired="false"
+              class="mb-4"
+            >
+            </AtomsFormBasicLabel>
+            <!-- 就業期間 -->
+            <div class="mb-6 flex items-center space-x-4">
+              <!-- 入社年月日 -->
+              <FormKit
+                type="text"
+                placeholder="2020/01"
+                inner-class="w-[160px] tb:w-[406px]"
+              >
+              </FormKit>
+              <div>〜</div>
+              <!-- 退社年月日 -->
+              <FormKit
+                type="text"
+                placeholder="2022/12"
+                inner-class="w-[160px] tb:w-[406px]"
+              >
+              </FormKit>
+            </div>
+            <!-- 会社名 -->
+            <div class="mb-6">
+              <FormKit type="text" placeholder="会社名" class="mb-6"></FormKit>
+            </div>
+            <!-- 部署・役職 -->
+            <div class="mb-6">
+              <FormKit
+                type="text"
+                placeholder="部署・役職"
+                class="mb-6"
+              ></FormKit>
+            </div>
+            <!-- 取り組み詳細 -->
+            <div class="mb-6">
+              <FormKit
+                type="textarea"
+                placeholder="取り組み詳細"
+                class="mb-6"
+              ></FormKit>
+            </div>
+          </div>
+          <!-- 職歴追加ボタン -->
+          <button
+            @click="clickAddRecentHistory"
+            v-show="recentHistoryCount < 3"
+            class="flex w-[164px] items-center justify-center rounded-lg border border-bancor-gray500 bg-bancor-gray1000 py-2 text-base"
+          >
+            職歴を追加 +
+          </button>
+        </div>
+
+        <!-- 履歴書・職務履歴書添付 -->
+        <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
+          <AtomsFormBasicLabel
+            text="履歴書・職務履歴書添付"
+            :isRequired="false"
+            class="mb-4"
+          >
+          </AtomsFormBasicLabel>
+          <!-- 履歴書・職務履歴書添付 -->
+          <div class="mb-4">
+            <FormKit type="file">
+              <template #input="context">
+                <button
+                  class="flex h-8 w-[164px] items-center justify-center rounded-lg border border-bancor-gray500 bg-bancor-gray600 py-2 text-base"
+                >
+                  ファイルを選択
+                  <input type="file" class="hidden" />
+                </button>
+              </template>
+            </FormKit>
+          </div>
+          <div>
+            <FormKit type="file">
+              <template #input="context">
+                <button
+                  class="flex h-8 w-[164px] items-center justify-center rounded-lg border border-bancor-gray500 bg-bancor-gray600 py-2 text-base"
+                >
+                  ファイルを選択
+                  <input type="file" class="hidden" />
+                </button>
+              </template>
+            </FormKit>
+          </div>
+        </div>
+
+        <!-- 区切り線 -->
+        <div class="mb-8 w-[360px] tb:w-[640px] pc:w-[860px]">
+          <div class="border-b border-bancor-gray500"></div>
+        </div>
+
+        <!-- 個人情報の取り扱いについての同意 -->
+        <div
+          class="mb-8 flex w-[360px] items-center justify-center tb:w-[640px] pc:w-[860px]"
+        >
+          <FormKit
+            type="checkbox"
+            label="個人情報"
+            name="terms"
+            :value="false"
+            validation="accepted"
+            validation-visibility="dirty"
+          >
+            <template #label="context">
+              <div class="flex items-center">
+                <span class="text-bancor-black400">
+                  <a
+                    href="http://localhost:3000/privacy"
+                    target="_blank"
+                    class="text-bancor-blue200"
+                    >個人情報</a
+                  >
+                </span>
+                <span class="text-bancor-black400">
+                  の取扱について同意する
+                </span>
+              </div>
+            </template>
+          </FormKit>
+        </div>
       </FormKit>
 
       <!-- フォーム送信ボタン -->
       <div
         @click="submitForm"
-        class="rounded-full bg-bancor-black400 py-[21px] px-[50px] text-sm font-bold text-white"
+        class="mb-24 rounded-full bg-bancor-black400 py-[21px] px-[50px] text-sm font-bold text-white"
       >
         同意の上、入力内容を確認
       </div>
