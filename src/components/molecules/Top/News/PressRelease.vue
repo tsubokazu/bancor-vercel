8537be0fa2fbd2419b3543c86ce9fd5d1ce039c0
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-04-05 20:15:07 +0900

    bugfix: ニュース記事を4記事のみに修正

--- src/components/molecules/Top/News/PressRelease.vue
+++ src/components/molecules/Top/News/PressRelease.vue
@@ -6,6 +6,8 @@
     await newsStore.fetchNews();
   }
   const { pressReleaseList }: TopPressReleaseList = newsStore;
+  // プレスリリースの4記事分をpressReleaseListから取得
+  const pressReleaseListLatest4 = computed(() => pressReleaseList.slice(0, 4));
 </script>
 
 <template>
@@ -13,14 +15,14 @@
     class="h-full w-full border-gray-300 px-4 pc:ml-8 pc:h-[486px] pc:w-[412px] pc:border-l pc:px-8"
   >
     <div class="mb-3 flex justify-between">
-      <div class="font-bold">{{ pressReleaseList[0].category }}</div>
+      <div class="font-bold">{{ pressReleaseListLatest4[0].category }}</div>
       <MoleculesViewMoreButton
         linkUrl="/news/press-release"
       ></MoleculesViewMoreButton>
     </div>
     <div class="flex flex-col space-y-4">
       <MoleculesArticleCard
-        v-for="card in pressReleaseList"
+        v-for="card in pressReleaseListLatest4"
         :key="card.subject"
         :card="card"
       ></MoleculesArticleCard>
--- src/components/molecules/Top/News/ServiceNews.vue
+++ src/components/molecules/Top/News/ServiceNews.vue
@@ -6,6 +6,8 @@
     await newsStore.fetchNews();
   }
   const { serviceNewsList }: TopServiceNewsList = newsStore;
+  // サービスニュースの4記事分をserviceNewsListから取得
+  const serviceNewsListLatest4 = computed(() => serviceNewsList.slice(0, 4));
 </script>
 
 <template>
@@ -13,14 +15,14 @@
     class="h-full w-full border-gray-300 px-4 tb:border-l pc:ml-8 pc:h-[486px] pc:w-[412px] pc:px-8"
   >
     <div class="mb-3 flex justify-between">
-      <div class="font-bold">{{ serviceNewsList[0].category }}</div>
+      <div class="font-bold">{{ serviceNewsListLatest4[0].category }}</div>
       <MoleculesViewMoreButton
         linkUrl="/news/service-news"
       ></MoleculesViewMoreButton>
     </div>
     <div class="flex h-full w-full flex-col space-y-4">
       <MoleculesArticleCard
-        v-for="card in serviceNewsList"
+        v-for="card in serviceNewsListLatest4"
         :key="card.subject"
         :card="card"
       ></MoleculesArticleCard>
