567da7c366f099f34e9d2c59f15242f1126bbf0e
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-04-16 11:07:59 +0900

    bugfix: 個人情報のリンク切れ修正とミニヘッダーのトップリンク追加

--- src/components/organisms/MiniHeader.vue
+++ src/components/organisms/MiniHeader.vue
@@ -2,9 +2,11 @@
 
 <template>
   <nav class="flex h-[80px] w-full items-center justify-center bg-white">
-    <AtomsBasicIcon
-      iconUrl="/logo-black-l.png"
-      size="h-[50px]"
-    ></AtomsBasicIcon>
+    <NuxtLink to="/">
+      <AtomsBasicIcon
+        iconUrl="/logo-black-l.png"
+        size="h-[50px]"
+      ></AtomsBasicIcon>
+    </NuxtLink>
   </nav>
 </template>
--- src/pages/bancor-3min.vue
+++ src/pages/bancor-3min.vue
@@ -293,7 +293,7 @@
                 <div class="flex items-center">
                   <span class="text-bancor-black400">
                     <a
-                      href="http://localhost:3000/privacy"
+                      href="http://localhost:3000/privacy-policy"
                       target="_blank"
                       class="text-bancor-blue200"
                       >個人情報</a
