c06bb19ea8dc4d9d2530e11d063f436b189ec67b
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-04-18 01:12:53 +0900

    update: Google Analyticsの設定

---
+++ src/plugins/analytics.client.ts
@@ -0,0 +1,18 @@
+import VueGtag from 'vue-gtag';
+
+// Nuxtプラグインの登録
+export default defineNuxtPlugin((nuxtApp) => {
+  // ルーター取得
+  const router = useRouter();
+
+  // Vue登録
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      appName: 'bancor.co.jp', // サイトの名称
+      pageTrackerScreenviewEnabled: true, // ページトラッキングスクリーンビューを有効
+      config: { id: `G-GV2HWN595B` }, // GoogleAnalytics(GA4)の測定IDを指定する
+    },
+    router
+  );
+});
