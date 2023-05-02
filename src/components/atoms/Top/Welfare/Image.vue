50e3159092709bb5942aa02cc7b9fbb30b663f8b
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-05-03 01:46:31 +0900

    bugfix: TOPページの福祉メニューの画像のサイズ修正

--- src/components/atoms/Top/Welfare/Image.vue
+++ src/components/atoms/Top/Welfare/Image.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-  <div class="h-fit w-fit rounded-sm tb:h-[292px] tb:w-[190px]">
+  <div class="h-[234px] w-[152px] rounded-sm tb:h-[292px] tb:w-[190px]">
     <img class="object-cover" :src="Props.imgUrl" />
   </div>
 </template>
