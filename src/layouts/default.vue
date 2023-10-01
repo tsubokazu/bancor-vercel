a2ad965728ae9e77371b222718d281b93a1f20a8
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-10-01 18:51:25 +0900

    fix: 初回アクセス時にmetaタグが機能していなかったので修正

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -34,6 +34,23 @@
   const { headTags } = bancorHeadTagStore as any | HeadTag[];
   let headTag: HeadTag = headTags[0];
 
+  useHead({
+    title: headTag.title,
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: headTag.description,
+      },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+  });
+
   const route = useRoute();
   watch(route, (newRoute, oldRoute) => {
     for (let i = 0; i < headTags.length; i++) {
