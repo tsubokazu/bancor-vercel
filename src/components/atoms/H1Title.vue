2def22321b30bb8b1cee24bc773f47514d3e636e
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2023-11-05 21:56:56 +0900

    feat: Bancor Journalのデザインアセット変更

---
+++ src/components/atoms/H1Title.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  interface Props {
+    text: string;
+    color?: string;
+    size?: string;
+    spaceY?: string;
+    isBold?: boolean;
+  }
+  const props = withDefaults(defineProps<Props>(), {
+    color: 'text-bancor-black100',
+    size: 'text-[28px] tb:text-[32px] pc:text-[40px]',
+    spaceY: 'space-y-5',
+    isBold: true,
+  });
+  const titleTexts = computed(() => props.text.split('\r\n'));
+</script>
+
+<template>
+  <h1
+    class="flex flex-col"
+    :class="[color, size, spaceY, { 'font-bold': isBold }]"
+  >
+    <div v-for="titleText in titleTexts" :key="titleText">
+      {{ titleText }}
+    </div>
+  </h1>
+</template>
--- src/components/molecules/BasicArticle.vue
+++ src/components/molecules/BasicArticle.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import { JournalObject } from '~/types/Journal';
+  import { load } from 'cheerio';
+  import axios from 'axios';
   const props = defineProps<{ journalObject: JournalObject }>();
   const instagramShareLink = `https://www.instagram.com/`;
   const twitterShareLink = `http://twitter.com/share?url=http://bancor.co.jp/journal/${props.journalObject.topicsId}`;
@@ -20,18 +22,105 @@
   const otherJournalList: Array<JournalObject> = devJournalList.filter(
     (journal) => journal.topicsId != props.journalObject.topicsId
   );
+
+  interface OGPData {
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+  }
+
+  const fetchOGP = async (url: string) => {
+    try {
+      // 指定されたURLからHTMLを取得
+      const { data } = await axios.get(url);
+
+      // cheerioでHTMLを読み込む
+      const $ = load(data);
+
+      // OGP情報を格納するオブジェクト
+      const ogp: Partial<OGPData> = {};
+
+      // metaタグからOGP情報を取得
+      $('meta').each((_, element) => {
+        const property = $(element).attr('property');
+        const content = $(element).attr('content');
+        if (property && content) {
+          if (property.includes('og:')) {
+            const key = property.split(':')[1] as keyof OGPData;
+            ogp[key] = content;
+          }
+        }
+      });
+
+      return {
+        title: ogp.title
+          ? ogp.title.length > 30
+            ? ogp.title.substring(0, 30) + '...'
+            : ogp.title
+          : '',
+        description: ogp.description
+          ? ogp.description.length > 30
+            ? ogp.description.substring(0, 30) + '...'
+            : ogp.description
+          : '',
+        image: ogp.image || '',
+        url: ogp.url || url, // 'og:url'が存在しない場合は、リクエストされたURLを使用
+      };
+    } catch (error) {
+      console.error('Error fetching OGP data:', error);
+      return null;
+    }
+  };
+
+  // HTMLの解析と修正を行う非同期関数
+  const parseAndModifyHTML = async (html: string): Promise<string> => {
+    const $ = load(html);
+
+    const promises = $('.nuvers-blog-card')
+      .map(async (_, element) => {
+        const card = $(element);
+        const urlText = card.find('.nuvers-blog-card-url').text().trim();
+        const ogpInfo = await fetchOGP(urlText);
+
+        if (ogpInfo) {
+          card.find('.nuvers-blog-card-title').text(ogpInfo.title);
+          card.find('.nuvers-blog-card-description').text(ogpInfo.description);
+          card
+            .find('.nuvers-blog-card-url')
+            .replaceWith(
+              `<a href="${urlText}" style="color:#8b8b8b;font-size:14px;">${urlText}</a>`
+            );
+          card
+            .find('.nuvers-blog-card-image')
+            .replaceWith(
+              `<div class="nuvers-blog-card-image" style="height:100%;width:30%;background-image:url('${ogpInfo.image}');background-size:cover;"></div>`
+            );
+        }
+      })
+      .get();
+
+    await Promise.all(promises);
+
+    return $.html();
+  };
+
+  // journalObject.bodyHTML内のclass="kuroco-article" の中のclass="nuvers-blog-card-url"からURLを取得
+  props.journalObject.bodyHTML = await parseAndModifyHTML(
+    props.journalObject.bodyHTML
+  );
 </script>
 
 <template>
   <div>
     <div class="flex h-full w-full flex-col space-y-16">
       <!-- タイトル -->
-      <AtomsBasicTitle
+      <AtomsH1Title
         :text="journalObject.subject"
         size="text-[28px]"
-      ></AtomsBasicTitle>
+      ></AtomsH1Title>
       <!-- 本文 -->
-      <div class="post prose-lg" v-html="journalObject.bodyHTML"></div>
+      <div class="kuroco-article" v-html="journalObject.bodyHTML"></div>
       <!-- SNS シェア -->
       <div class="flex h-12 w-10/12 items-center justify-between pl-3">
         <NuxtLink :to="instagramShareLink">
