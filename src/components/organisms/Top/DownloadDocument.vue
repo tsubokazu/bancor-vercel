e19b5d02f85c22c6bdfb7e6e4dcee0c1e5f9b826
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2024-01-13 11:01:43 +0900

    fix: TOPページの採用・資料コンテンツのレスポンシブ対応

--- src/components/organisms/Top/DownloadDocument.vue
+++ src/components/organisms/Top/DownloadDocument.vue
@@ -3,16 +3,18 @@
 <template>
   <div class="relative flex w-full items-center justify-center pt-24">
     <div
-      class="flex w-11/12 items-center justify-between space-x-10 pc:max-w-[1300px]"
+      class="flex w-11/12 flex-col items-center justify-between gap-6 tb:flex-row tb:gap-0 tb:space-x-10 pc:max-w-[1300px]"
     >
       <!-- メッセージコンテンツ -->
       <div class="flex flex-col">
         <!-- アイコン -->
-        <div class="h-[100px] w-[76px]">
+        <div class="h-[50px] w-[38px] pc:h-[100px] pc:w-[76px]">
           <img src="/top-document-icon.png" alt="" />
         </div>
         <!-- メッセージ -->
-        <div class="mt-4 text-[40px] font-bold text-[#94a3b8]">
+        <div
+          class="mt-4 text-[20px] font-bold text-[#94a3b8] tb:text-[24px] pc:text-[40px]"
+        >
           NUVO M&A<span class="text-[#020617]">独自の価値基準</span
           >に基づき<br />
           プロダクトを<span class="text-[#020617]">スピーディーに改善。</span
@@ -22,7 +24,7 @@
         <!-- ダウンロードページリンク -->
         <NuxtLink
           to="/materials/130"
-          class="mt-[72px] flex items-center gap-2 text-[18px] text-[#94a3b8] transition-colors duration-300 hover:text-[#020617]"
+          class="mt-[20px] flex items-center gap-2 text-[14px] text-[#94a3b8] transition-colors duration-300 hover:text-[#020617] pc:mt-[72px] pc:text-[18px]"
         >
           <div>ダウンロード</div>
           <font-awesome-icon :icon="['fas', 'download']" />
--- src/components/organisms/Top/Recruit.vue
+++ src/components/organisms/Top/Recruit.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="relative flex w-full items-center justify-center pt-24">
     <div
-      class="flex w-11/12 items-center justify-between space-x-10 pc:max-w-[1300px]"
+      class="flex w-11/12 flex-col-reverse items-center justify-between gap-6 tb:flex-row tb:gap-0 tb:space-x-10 pc:max-w-[1300px]"
     >
       <!-- 画像イラスト -->
       <div class="w-full max-w-[631px] tb:w-1/2 pc:h-[468px]">
@@ -18,7 +18,7 @@
           Bancorは、一緒に挑戦する仲間を募集しています。<br />
           ビジョン、会社紹介、募集職種などさまざまな情報を公開していますのでぜひご確認ください。
         </div>
-        <!-- ダウンロードページリンク -->
+        <!-- 採用ページリンク -->
         <NuxtLink
           to="/recruit-dx"
           class="group mt-10 flex items-center gap-3 text-[#94a3b8] transition-colors duration-300 hover:text-[#020617]"
