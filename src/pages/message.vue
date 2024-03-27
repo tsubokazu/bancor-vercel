d230b3b9680c8cd2a803fce9f6a4f28c30af5f65
tsubokazu <83831401+tsubokazu@users.noreply.github.com>
2024-03-27 23:23:04 +0900

    feat: トップメッセージのデザイン修正

--- src/pages/message.vue
+++ src/pages/message.vue
@@ -2,6 +2,11 @@
   import { MessageObject } from '~/types/pages/message';
   import { usePagesMessageStore } from '~/stores/pages/message';
 
+  // layoutを指定
+  definePageMeta({
+    layout: 'company',
+  });
+
   useHead({
     script: [
       {
@@ -20,23 +25,96 @@
 </script>
 
 <template>
-  <div class="relative flex w-full flex-col items-center bg-[#f1f5f9]">
+  <div
+    class="relative flex w-full flex-col items-center justify-between overflow-hidden"
+  >
+    <!-- 背景 -->
+    <div class="absolute top-0 left-0 -z-50 h-full w-full bg-[#f1f5f9]"></div>
     <!-- ヘッダー -->
-    <div
-      class="h-[240px] w-[95%] pc:h-[320px] pc:w-full pc:max-w-[1460px]"
-    ></div>
+    <div class="flex h-[180px] w-[95%] tb:h-[320px] pc:max-w-[1460px]">
+      <div class="mt-[100px] flex flex-col gap-1 tb:mt-[160px] tb:gap-3">
+        <!-- 小さなタイトル -->
+        <div class="text-[14px] font-bold tb:text-[16px]">トップメッセージ</div>
+        <!-- 大きなタイトル -->
+        <AtomsFuturaBoldText
+          text="TOP Message"
+          size="text-[28px] tb:text-[40px]"
+        />
+      </div>
+    </div>
     <!-- 背景アニメーション -->
     <div
-      class="h-[240px] w-[95%] pc:h-[360px] pc:w-full pc:max-w-[1460px]"
-    ></div>
+      class="absolute top-[-130px] right-[-400px] -z-10 h-[731px] w-[960px] overflow-hidden tb:right-[-150px]"
+    >
+      <video autoplay loop muted playsinline class="h-full w-full object-cover">
+        <source src="/movies/sc.mp4" type="video/mp4" />
+      </video>
+    </div>
     <!-- ボディ -->
-    <div class="flex w-[95%] pc:w-full pc:max-w-[1460px]">
+    <div
+      class="flex w-[95%] flex-col gap-4 tb:flex-row pc:max-w-[1460px] pc:gap-8"
+    >
       <!-- サイド -->
       <OrganismsCompanyInfoSideMenu />
       <!-- メイン -->
-      <div
-        class="relative mb-40 flex w-full flex-col items-center space-y-24"
-      ></div>
+      <div class="mb-[128px] flex w-full flex-col gap-14">
+        <div
+          class="relative flex w-full flex-col items-center rounded-[10px] border border-[#cbd5e1] bg-white pt-10 pc:pt-[100px]"
+        >
+          <!-- 画像 -->
+          <div
+            class="relative mb-16 flex h-[300px] w-[95%] items-center justify-center overflow-hidden rounded-[10px] pc:h-[472px] pc:max-w-[1000px]"
+          >
+            <img
+              :src="message.imgUrl"
+              alt="トップメッセージ"
+              class="h-full w-full object-cover"
+            />
+            <!-- ロゴ -->
+            <div
+              class="absolute top-10 left-0 h-[100px] pc:top-[200px] pc:h-[100px]"
+            >
+              <img
+                src="/logo-all-white.png"
+                alt="ロゴ"
+                class="h-full w-full object-cover"
+              />
+            </div>
+            <!-- 注釈 -->
+            <div class="absolute bottom-2 right-2 flex flex-col opacity-90">
+              <AtomsFuturaBoldText
+                text="Bancor Inc."
+                size="text-[12px] tb:text-[15px]"
+                color="text-white"
+              />
+              <div class="text-[12px] text-white tb:text-[15px]">
+                代表取締役社長　山口 龍成
+              </div>
+            </div>
+          </div>
+          <!-- メインメッセージ -->
+          <div class="mb-16 flex w-[95%] pc:max-w-[1000px]">
+            <div
+              class="text-[22px] font-bold text-[#2563eb] tb:text-[30px] pc:text-[60px]"
+            >
+              {{ message.mainMessage }}
+            </div>
+          </div>
+          <!-- メッセージ本文 -->
+          <div class="flex w-[95%] pc:max-w-[1000px]">
+            <div class="post prose" v-html="message.message"></div>
+          </div>
+        </div>
+        <!-- パンくずリスト -->
+        <div
+          class="mb-[128px] flex w-full items-center gap-2 rounded-xl border border-[#cbd5e1] py-5 px-6 font-bold text-[#101010]"
+        >
+          <font-awesome-icon :icon="['fas', 'house']" class="text-[10px]" />
+          <NuxtLink to="/" class="text-xs">TOP</NuxtLink>
+          <div class="text-xs">></div>
+          <NuxtLink to="/message" class="text-xs">トップメッセージ</NuxtLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
